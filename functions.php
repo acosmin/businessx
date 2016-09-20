455ce1fcdec9f833c449be02a373e2f815fc201e
acosmin <acosmin.com@gmail.com>
2016-09-20 18:19:03 +0300

    Changed version to 1.0.3.3

--- functions.php
+++ functions.php
@@ -15,7 +15,7 @@
  *  Define some constants
 /* ------------------------------------ */
 if( ! defined( 'BUSINESSX_VERSION' ) ) {
-	define( 'BUSINESSX_VERSION', '1.0.3.2' ); }
+	define( 'BUSINESSX_VERSION', '1.0.3.3' ); }
 
 if( ! defined( 'BUSINESSX_AC_URL' ) ) {
 	define( 'BUSINESSX_AC_URL', 'http://www.acosmin.com/' ); }
