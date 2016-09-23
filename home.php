85e75b114278752883af990e0c7dd9c4c611c445
acosmin <acosmin.com@gmail.com>
2016-09-23 11:25:09 +0300

    Spacing

--- home.php
+++ home.php
@@ -1,6 +1,6 @@
 <?php
 /* ------------------------------------------------------------------------- *
- *	Home/Index template					
+ *	Home/Index template
 /* ------------------------------------------------------------------------- */
 
 /*  Hooked CSS classes
@@ -20,33 +20,33 @@ businessx_get_heading_templ( 'index', 'full-width' );
 
 <section role="main" id="content" class="<?php businessx_occ( 'businessx_index___section_classes' ); ?>">
 	<?php do_action( 'businessx_index__inner_sec_top' ); ?>
-    
+
 	<div class="<?php businessx_occ( 'businessx_index___container_classes' ); ?>">
-    	
+
         <?php do_action( 'businessx_index__inner_before' ); ?>
-        
+
         <main id="main" class="<?php businessx_occ( 'businessx_index___main_classes' ); ?>" role="main">
-			<?php 
-			if ( have_posts() ) : 
-				while ( have_posts() ) : the_post(); 
+			<?php
+			if ( have_posts() ) :
+				while ( have_posts() ) : the_post();
 					get_template_part( 'partials/posts/content', get_post_format() );
 				endwhile;
 				businessx_paged( '<nav class="posts-pagination grid-col grid-4x-col fw-bold ta-right clearfix" role="navigation">' );
 			else :
 				get_template_part( 'partials/posts/content', 'none' );
 			endif;
-			?>	
+			?>
         </main>
-		
+
         <?php get_sidebar(); ?>
-        
+
         <?php do_action( 'businessx_index__inner_after' ); ?>
-        
+
     </div>
-    
+
     <?php do_action( 'businessx_index__inner_sec_bottom' ); ?>
 </section>
 
-<?php 
+<?php
 // Footer
 get_footer(); ?>
