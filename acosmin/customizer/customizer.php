f6b27a0330f032f2aac632a682942a074cf8fdce
acosmin <acosmin.com@gmail.com>
2016-09-29 12:11:33 +0300

    Changed description for "Logo Type"

--- acosmin/customizer/customizer.php
+++ acosmin/customizer/customizer.php
@@ -1140,7 +1140,7 @@ function businessx_customize_register( $wp_customize ) {
 				'logo-text-type' 	=> esc_html__( 'Text', 'businessx' ),
 			) ),
 			__( 'Logo type:', 'businessx' ),
-			__( 'Select whether to use a custom logo or your site title.', 'businessx' ),
+			__( 'Select whether to use a custom logo or your site title. If a logo is not selected, the text one is displayed.', 'businessx' ),
 			apply_filters( 'businessx_option___logo_type_select_default', 'logo-text-type' ), false, 'sanitize_text_field', 0 );
 		/*=====*/
 
