45fa7a789c3c1841cf14845a08d89ab2255a9399
acosmin <acosmin.com@gmail.com>
2017-04-30 13:53:20 +0300

    Updated Customizer documentation button

--- acosmin/customizer/controls/link/link.php
+++ acosmin/customizer/controls/link/link.php
@@ -14,11 +14,13 @@ if( ! class_exists( 'Businessx_Section_Link' ) ) {
 		 * Custom button text to output.
 		 */
 		public $link_text = '';
+		public $rate_text = '';
 
 		/**
 		 * Custom pro button URL.
 		 */
 		public $link_url = '';
+		public $rate_url = '';
 
 		/**
 		 * Add custom parameters to pass to the JS via JSON.
@@ -28,6 +30,8 @@ if( ! class_exists( 'Businessx_Section_Link' ) ) {
 
 			$json['link_text'] = $this->link_text;
 			$json['link_url']  = esc_url( $this->link_url );
+			$json['rate_text'] = $this->rate_text;
+			$json['rate_url']  = esc_url( $this->rate_url );
 
 			return $json;
 		}
@@ -40,10 +44,12 @@ if( ! class_exists( 'Businessx_Section_Link' ) ) {
 			<li id="accordion-section-{{ data.id }}" class="accordion-section control-section control-section-{{ data.type }} cannot-expand">
 
 				<h3 class="accordion-section-title">
-					{{ data.title }}
-
 					<# if ( data.link_text && data.link_url ) { #>
-						<a href="{{ data.link_url }}" class="button button-secondary alignright" target="_blank">{{ data.link_text }}</a>
+						<a href="{{ data.link_url }}" class="button bx-docs-btn button-secondary alignleft" target="_blank"><i class="fa fa-info-circle"></i>&nbsp;{{ data.link_text }}</a>
+					<# } #>
+					&nbsp;
+					<# if ( data.rate_text && data.rate_url ) { #>
+						<a href="{{ data.rate_url }}" class="button bx-rate-btn button-secondary alignright" target="_blank"><i class="fa fa-star"></i>&nbsp;{{ data.rate_text }}</a>
 					<# } #>
 				</h3>
 			</li>
